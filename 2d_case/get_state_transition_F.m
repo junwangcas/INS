0b2199416ac11296a4e69ec68958854457561f3f
avicvision <avicvision@163.com>
2019-06-09 21:24:07 +0800

    fix some 2d case bug.

--- 2d_case/get_state_transition_F.m
+++ 2d_case/get_state_transition_F.m
@@ -1,13 +1,12 @@
-function [fx, F] = get_state_transition_F(delta_t, yawrate,a_L,theta_t_1,v_t_1,s_t_1,bias)
-theta_t = theta_t_1 + (yawrate+bias)*delta_t;
+function [fx, F] = get_state_transition_F(delta_t, yawrate,a_L,theta_t_1,v_t_1,s_t_1)
+theta_t = theta_t_1 + (yawrate)*delta_t;
 theta_t = normalize_theta(theta_t);
 v_t = v_t_1 + to_R2d(theta_t_1)*a_L*delta_t;
 s_t = s_t_1 + v_t_1*delta_t+ 1/2*to_R2d(theta_t_1)*a_L*delta_t*delta_t;
-fx =[theta_t;v_t;s_t;bias];
+fx =[theta_t;v_t;s_t];
 
 dR_theta = [-sin(theta_t_1),-cos(theta_t_1);cos(theta_t_1),-sin(theta_t_1)];
-F = eye(6,6);
-F(1,6) = delta_t;
+F = eye(5,5);
 F(2:3, 1) = dR_theta*a_L*delta_t;
 F(4:5, 1) =1/2*dR_theta*a_L*delta_t*delta_t;
 F(4:5, 2:3) = eye(2,2)*delta_t;
